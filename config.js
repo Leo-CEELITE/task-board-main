2e29c8133c8e196d55b7635444feffc49b63c39b
Leo-CEELITE <leo@ceelite-immigration.com>
2026-05-27 20:54:59 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -11,8 +11,8 @@
 //   Open your Master Task Board Google Sheet and copy the full URL from the browser.
 
 window.CE_CONFIG = {
-  API_URL:   "https://script.google.com/macros/s/YOUR_CE_DEPLOYMENT_ID/exec",
-  API_TOKEN: "ce-task-api-YOUR_SECRET_TOKEN",
-  PASSWORD:  "YOUR_CE_PASSWORD",
-  SHEET_URL: "https://docs.google.com/spreadsheets/d/YOUR_CE_SHEET_ID/edit"
+  API_URL:   "https://script.google.com/macros/s/AKfycbzLSvsaKORyEjtYm7BlgBubXGokO5ns7blduzTsc8AiGxOD5UdgrP_BjreJbwJCuaKYcQ/exec",
+  API_TOKEN: "CE_Task_Super_Secret_2026",
+  PASSWORD:  "888CE",
+  SHEET_URL: "https://docs.google.com/spreadsheets/d/1P6JvkMk5rWH1rsde_YO0AQa6GZ3U8FNzIQY2HNKsCYQ/edit?gid=0#gid=0"
 };
